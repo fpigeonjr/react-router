3b69c3e11ad4aad7bcf38155575824301b7d1270
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-07 22:12:26 -0600

    adds CourseContainer and adds passing data to Components Via Routes.

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { NavLink, Route, Redirect } from "react-router-dom";
-import CSS from "./courses/CSS";
-import HTML from "./courses/HTML";
-import Javascript from "./courses/JavaScript";
+import CourseContainer from "./courses/CourseContainer";
+import { HTMLCourses, CSSCourses, JSCourses } from "../data/courses";
 
 const Courses = ({ match }) => (
   <div className="main-content courses">
@@ -26,9 +25,18 @@ const Courses = ({ match }) => (
       path={match.path}
       render={() => <Redirect to={`${match.path}/html`} />}
     />
-    <Route path={`${match.path}/html`} component={HTML} />
-    <Route path={`${match.path}/css`} component={CSS} />
-    <Route path={`${match.path}/javascript`} component={Javascript} />
+    <Route
+      path={`${match.path}/html`}
+      render={() => <CourseContainer data={HTMLCourses} />}
+    />
+    <Route
+      path={`${match.path}/css`}
+      render={() => <CourseContainer data={CSSCourses} />}
+    />
+    <Route
+      path={`${match.path}/javascript`}
+      render={() => <CourseContainer data={JSCourses} />}
+    />
   </div>
 );
 
